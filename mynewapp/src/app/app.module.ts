2e73c678370a356c4ab836516d6d4504bf055d73
ankur300 <ankurchoudhary300@gmail.com>
2021-02-13 19:10:33 +0530

    image addtion

--- mynewapp/src/app/app.module.ts
+++ mynewapp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TopnavComponent } from './navbar/topnav/topnav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TopnavComponent
+    TopnavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
